fae417f991c684be7033cdfb465b136a998f0089
saho_ikeda <saho.ikeda0223@gmail.com>
2021-04-24 20:00:19 +0900

    # エラー抽出

--- 4-3/main.js
+++ 4-3/main.js
@@ -1,5 +1,12 @@
-const errorLogs = [
+/const errorLogs = [
   "Error 101:Invalid Exception AAA - 2020/01/01/01:00",
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
-]
+];
+// 正規表現
+for (let i=0; i<errorLogs.length; i++) {
+  let get_str = errorLogs[i];
+  let found = get_str.match(/:.*\s[-]/g);
+  let edit = String(found).replace(/:/g, "").replace(/\s[-]/g, "");
+  console.log(edit);
+}
