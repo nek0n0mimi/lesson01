d86c1518c27b2304aa891f127884e2eb453ff565
saho_ikeda <saho.ikeda0223@gmail.com>
2021-04-25 14:05:11 +0900

    # 数字の文字列操作

---
+++ 4-6/main.js
@@ -0,0 +1,15 @@
+const arg = process.argv[2];
+
+const times = arg.split(":");
+const minutes = Number(times[1]) + 45;
+
+if (minutes>=60 && times[0]==23) {
+  console.log("0");
+  console.log(String(minutes-60));
+} else if (minutes<=60){
+  console.log(times[0]);
+  console.log(String(minutes));
+} else {
+  console.log(String(Number(times[0])+1));
+  console.log(String(minutes-60));
+}
