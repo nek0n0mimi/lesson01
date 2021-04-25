fa53280f04190abfb0ef93ffb8cbe061a1ad4f6b
nek0n0mimi <68776561+nek0n0mimi@users.noreply.github.com>
2021-04-25 16:11:28 +0900

    Update main.js

--- 4-6/main.js
+++ 4-6/main.js
@@ -3,8 +3,8 @@ const arg = process.argv[2];
 const times = arg.split(":");
 const minutes = Number(times[1]) + 45;
 
-if (minutes>=60 && times[0]==23) {
-  console.log("0");
+if (minutes>=60 && times[0]>=23) {
+  console.log(String(times[0]-23));
   console.log(String(minutes-60));
 } else if (minutes<=60){
   console.log(times[0]);
